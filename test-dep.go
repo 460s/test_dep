821320c764c9b8143999f6d618673c659f7fb6d9
Dmitriy Syrovatskiy <d.syrovatskiy@ispsystem.com>
2020-01-14 12:42:39 +0800

    1.0.0

--- test-dep.go
+++ test-dep.go
@@ -4,10 +4,6 @@ import (
 	"fmt"
 )
 
-func TestDep()  {
-	fmt.Println("Hello i'm testDep 0.0.4")
-}
-
 func TestDep2()  {
-	fmt.Println("Hello i'm testDep2 0.0.4")
+	fmt.Println("Hello i'm testDep2 1.0.0")
 }
